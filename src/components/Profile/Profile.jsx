c41cc325d5d30eb6cb55f3dd1a40a32b9a6f4d7e
Olha <oyashnaya88@gmail.com>
2024-12-04 01:19:13 +0200

    css

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -9,11 +9,11 @@ const Profile = ({
 }) => {
   return (
     <div className={css.profileContainer}>
-      <div>
+      <div className={css.profileItems}>
         <img className={css.profileImg} src={image} alt="User avatar" />
-        <p className={css.profileName}>{name}</p>
-        <p className={css.profileTag}>{tag}</p>
-        <p className={css.profileLocation}>{location}</p>
+        <p className={css.profileTitle}>{name}</p>
+        <p className={css.profileSubtitle}>{tag}</p>
+        <p className={css.profileSubtitle}>{location}</p>
       </div>
 
       <ul className={css.profileList}>
